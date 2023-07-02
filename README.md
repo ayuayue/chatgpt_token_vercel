5ff3b453c8ba4a92a8de18e7b773fae65e86438e
caoayu <1401262639@qq.com>
2023-07-03 00:26:53 +0800

    📦 NEW:

--- README.md
+++ README.md
@@ -6,4 +6,17 @@
 
 所有的接口都要在 api 目录下。
 
+| 接口 | 说明 |
+|---------|---------|
+| / | 官网直登获取 Token 功能页面 |
+| /third | 第三方账号获取 Token 页面 |
+| /getOpenAItoken | 官网直登获取 Token 接口 |
+| /getOpenAItokenByCode | 通过 Code 获取 Token 接口 |
+| /revokeOpenAIToken | 撤销 refresh_token 接口 |
+| /refreshOpenAIToken | 刷新 Token 接口 |
+
+
 部署地区：regions ：sin1 新加坡
+
+
+如果要部署官网直登的api接口，需要在 vercel 上设置环境变量，key 是 LoginURL,值就是自己部署的 pandora 项目域名，注意不要用 cf 的安全模式，不然没法直接调用，因为安全问题需要手动验证。
---
+++ api/get_login_openai_token.go
@@ -0,0 +1,72 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"regexp"
+	"os"
+	"net/url"
+)
+
+func GetLoginToken(w http.ResponseWriter, r *http.Request) {
+	loginUrl := os.Getenv("LoginURL")+"/auth/login"
+	// loginUrl = "https://pandora.caoayu.top/auth/login"
+	fmt.Println(loginUrl)
+	method := "POST"
+	params := url.Values{}
+	params.Set("username", r.PostFormValue("username"))
+	params.Set("password", r.PostFormValue("password"))
+	params.Set("mfa_code", r.PostFormValue("mfa_code"))
+	params.Set("action", "default")
+	params.Set("next", "")
+	payload := strings.NewReader(params.Encode())
+	fmt.Println(params.Encode())
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	req, err := http.NewRequest(method, loginUrl, payload)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	req.Header.Add("User-Agent", "apifox/1.0.0 (https://www.apifox.cn)")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+	defer res.Body.Close()
+
+
+	// 获取重定向返回的 header
+	redirectHeaders := res.Header
+
+	// 将 header 转换为字符串
+	var sb strings.Builder
+	for key, values := range redirectHeaders {
+		for _, value := range values {
+			sb.WriteString(key)
+			sb.WriteString(": ")
+			sb.WriteString(value)
+			sb.WriteString("\r\n")
+		}
+	}
+	redirectHeadersStr := sb.String()
+
+
+	// 使用正则表达式匹配 access_token 的内容
+	regex := regexp.MustCompile(`access-token=(.*?);`)
+	matches := regex.FindStringSubmatch(redirectHeadersStr)
+
+	if len(matches) > 1 {
+		accessToken := matches[1]
+		fmt.Fprint(w, "Access Token:"+accessToken)
+	} else {
+		fmt.Fprint(w, "Access Token not found in the response.")
+	}
+}
